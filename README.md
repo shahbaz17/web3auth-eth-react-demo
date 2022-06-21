17e60025bb2af6e6f69dea49a43f243340be2bfb
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-06-21 13:19:47 +0530

    Web3Auth-ETH-React Demo App

--- README.md
+++ README.md
@@ -1,70 +1,19 @@
-# Getting Started with Create React App
+# Web3Auth x ETH x React Demo Application
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Quick Start
 
-## Available Scripts
+```bash
+git clone https://github.com/shahbaz17/web3auth-eth-react-demo.git
+cd web3auth-eth-react-demo
+mv .env.example .env.local
+# enter REACT_APP_CLIENT_ID from https://dashboard.web3auth.io
+# enter REACT_APP_INFURA_ID from https://infura.io
+npm install
+npm start
+```
 
-In the project directory, you can run:
+## Guide
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[Read the full guide](https://web3auth.io/docs/guides/react) that covers the
+basics of how to set up your [Web3Auth](https://web3auth.io) SDK to start using
+Ethereum Blockchain in a React Application.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import {Web3Auth} from '@web3auth/web3auth'
+import Web3 from 'web3'
+import './App.css'
+
+const clientId = process.env.REACT_APP_CLIENT_ID // get the clientId from https://dashboard.web3auth.io
 
 function App() {
+  const [web3auth, setWeb3auth] = useState(null)
+  const [provider, setProvider] = useState(null)
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3auth = new Web3Auth({
+          clientId,
+          chainConfig: {
+            chainNamespace: 'eip155',
+            chainId: '0x3', // ropsten, use '0x1' if don't want to use Infura/Ropsten.
+            rpcTarget: `https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_ID}`, // use 'https://rpc.ankr.com/eth' if don't want to use Infura/Ropsten.
+          },
+        })
+
+        setWeb3auth(web3auth)
+
+        await web3auth.initModal()
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    init()
+  }, [])
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    const web3authProvider = await web3auth.connect()
+    setProvider(web3authProvider)
+  }
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    const user = await web3auth.getUserInfo()
+    console.log(user)
+    uiConsole(user)
+  }
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole('web3auth not initialized yet')
+      return
+    }
+    await web3auth.logout()
+    setProvider(null)
+  }
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const web3 = new Web3(provider)
+    const userAccounts = await web3.eth.getAccounts()
+    uiConsole(userAccounts)
+  }
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const web3 = new Web3(provider)
+    const accounts = await web3.eth.getAccounts()
+    const balance = await web3.eth.getBalance(accounts[0])
+    uiConsole(web3.utils.fromWei(balance) + ' ETH')
+  }
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const web3 = new Web3(provider)
+    // Get user's Ethereum public address
+    const fromAddress = (await web3.eth.getAccounts())[0]
+
+    const originalMessage = 'Your Message'
+
+    const signedMessage = await web3.eth.personal.sign(
+      originalMessage,
+      fromAddress,
+    )
+    uiConsole(signedMessage)
+  }
+
+  const signTransaction = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const web3 = new Web3(provider)
+    const accounts = await web3.eth.getAccounts()
+    const txRes = await web3.eth.signTransaction({
+      from: accounts[0],
+      to: accounts[0],
+      value: web3.utils.toWei('0.01'),
+    })
+    uiConsole(txRes.raw)
+  }
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole('provider not initialized yet')
+      return
+    }
+    const web3 = new Web3(provider)
+    const accounts = await web3.eth.getAccounts()
+
+    const txRes = await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: accounts[0],
+      value: web3.utils.toWei('0.0001'),
+    })
+    uiConsole(txRes.transactionHash)
+  }
+
+  function uiConsole(...args) {
+    const el = document.querySelector('#console>p')
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2)
+    }
+  }
+
+  const loggedInView = (
+    <>
+      <button onClick={getUserInfo} className="card">
+        Get User Info
+      </button>
+      <button onClick={getAccounts} className="card">
+        Get Accounts
+      </button>
+      <button onClick={getBalance} className="card">
+        Get Balance
+      </button>
+      <button onClick={signMessage} className="card">
+        Sign Message
+      </button>
+      <button onClick={signTransaction} className="card">
+        Sign Transaction
+      </button>
+      <button onClick={sendTransaction} className="card">
+        Send Transaction
+      </button>
+
+      <button onClick={logout} className="card">
+        Log Out
+      </button>
+
+      <div id="console" style={{whiteSpace: 'pre-line'}}>
+        <p style={{whiteSpace: 'pre-line'}}></p>
+      </div>
+    </>
+  )
+
+  const unloggedInView = (
+    <button onClick={login} className="card">
+      Login
+    </button>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="container">
+      <h1 className="title">
+        <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
+          Web3Auth
+        </a>{' '}
+        & ReactJS Example using Ethereum
+      </h1>
+
+      <div className="grid">{provider ? loggedInView : unloggedInView}</div>
+
+      <footer className="footer">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Web3Auth/Web3Auth/tree/master/examples/react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source code
         </a>
-      </header>
+      </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
